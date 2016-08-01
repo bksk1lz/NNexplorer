1626bbb6981da93d1f373979206c25d1773e4e66
bksk1lz <bcking21@gmail.com>
2016-08-01 10:41:35 -0400

    added a 2nd comment in something

--- fullapp.py
+++ fullapp.py
@@ -10,6 +10,7 @@ from bokeh.models.widgets import Slider, TextInput, RadioButtonGroup, Dropdown
 from bokeh.plotting import figure
 
 #a comment!
+#and a 2nd comment!!!!
 filelist = ['data/' + f for f in os.listdir('data') if f.endswith('.xls')]
 
 def treequerycalc(df, k):
