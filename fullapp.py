477e1497f3fecd6ed1ed7e00b8231ee7fdd72a3e
bksk1lz <bcking21@gmail.com>
2016-08-01 10:33:36 -0400

    inserted a comment

--- fullapp.py
+++ fullapp.py
@@ -9,6 +9,7 @@ from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import Slider, TextInput, RadioButtonGroup, Dropdown
 from bokeh.plotting import figure
 
+#a comment!
 filelist = ['data/' + f for f in os.listdir('data') if f.endswith('.xls')]
 
 def treequerycalc(df, k):
